d1b2d929d88019cecf2024c2b557f5dddee0a8a2
xtmx <xtmx@xtmxs-MacBook-Pro.local>
2022-08-05 18:08:39 -0300

    first, last page + error handling

--- parser/helpers.py
+++ parser/helpers.py
@@ -1,8 +1,18 @@
 import cv2
 import numpy as np
 import pdf2image
-import pandas as pd
 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+def show(im):
+    plotting = plt.imshow(im,cmap='gray')
+    plt.show()
+
+def crop_section(intial_width,intial_height,crop_width,crop_height,im):
+    #return im[intial_height:intial_height+crop_height, intial_width:intial_width+crop_width]
+    area = (intial_width, intial_height, intial_width+crop_width, intial_height+crop_height)
+    return im.crop(area)
 
 def strip_lower(text):
     try:
@@ -10,13 +20,12 @@ def strip_lower(text):
     except:
         return text
 
-def items_to_csv(items, output_csv_path):
+def items_to_csv(items, output_path):
     # Convert dictionary to csv file trough pandas df
     df = pd.DataFrame.from_dict(items)
-    df.to_csv (output_csv_path, index = False, header=True) 
+    df.to_csv (output_path, index = False, header=True) 
 
 def pdf_to_img(pdf_file_path, dpi=200,page=(None,None)) :
-
     PDF_PATH = pdf_file_path
     DPI = dpi
     #OUTPUT_FOLDER = output_images_path
@@ -28,32 +37,26 @@ def pdf_to_img(pdf_file_path, dpi=200,page=(None,None)) :
     USE_CROPBOX = False
     STRICT = False
 
-    def pdftopil():
+    return pdf2image.convert_from_path(
+            PDF_PATH,
+            dpi=DPI,
+            #output_folder=OUTPUT_FOLDER,
+            first_page=FIRST_PAGE,
+            last_page=LAST_PAGE,
+            fmt=FORMAT,
+            thread_count=THREAD_COUNT,
+            userpw=USERPWD,
+            use_cropbox=USE_CROPBOX,
+            strict=STRICT
+        )
 
-        pil_images = pdf2image.convert_from_path(PDF_PATH,
-                                                 dpi=DPI,
-                                                 #output_folder=OUTPUT_FOLDER,
-                                                 first_page=FIRST_PAGE,
-                                                 last_page=LAST_PAGE,
-                                                 fmt=FORMAT,
-                                                 thread_count=THREAD_COUNT,
-                                                 userpw=USERPWD,
-                                                 use_cropbox=USE_CROPBOX,
-                                                 strict=STRICT)
-
-        return pil_images
-
-    pil_images = pdftopil()
-
-    return pil_images
 
 def get_countours(im):
     ret,thresh = cv2.threshold(im,180,255,cv2.THRESH_BINARY_INV)# + cv2.THRESH_OTSU)
     kernel = np.ones((3,3),np.uint8)
     dilated = cv2.dilate(thresh,kernel,iterations = 1)
-    #blur = cv2.GaussianBlur(thresh, (3,3), cv2.BORDER_DEFAULT)
     contours, hierarchy = cv2.findContours(dilated,cv2.RETR_TREE ,cv2.CHAIN_APPROX_SIMPLE)
-    return contours#, hierarchy
+    return contours
 
 def crop(im, contours, hh, ww):
     processed = []
@@ -66,60 +69,43 @@ def crop(im, contours, hh, ww):
 
     return processed
 
-def resize_img(img, scale):
-    width = int(img.shape[1] * scale)
-    height = int(img.shape[0] * scale)
-    dim = (width, height)
+# def resize_img(img, scale):
+#     width = int(img.shape[1] * scale)
+#     height = int(img.shape[0] * scale)
+#     dim = (width, height)
     
-    # resize image
-    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+#     return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
 def remove_contours(im, contours, hh):
-
-    #test_im = np.ones(im.shape[:2], dtype=np.uint8)
-    #mask = np.ones(im.shape[:2], dtype="uint8") * 255
     ret,thresh = cv2.threshold(im,180,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     processed = thresh
 
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         
-        if h > hh[0] and h < hh[1]:         
-            #processed = cv2.rectangle(processed,(x+3,y+3),(x+w-3,y+h-3),(255,255,255),5)
-            #processed = cv2.rectangle(processed,(x-3,y-3),(x+w+3,y+h+3),(255,255,255),5)
-            #processed = cv2.rectangle(processed,(x,y),(x+w,y+h),(255,255,0),thickness=5)
-
-            z = 6 #6
+        if h > hh[0] and h < hh[1]:
+            z = 6 
             processed = cv2.line(processed,(x,y),(x+w,y),(0,0,0),10)
             processed = cv2.line(processed,(x+w-z,y),(x+w-z,y+h),(0,0,0),12)
             processed = cv2.line(processed,(x+z,y),(x+z,y+h),(0,0,0),12)
             processed = cv2.line(processed,(x,y+h-z),(x+w,y+h-z),(0,0,0),10)
 
-            # cv2.imshow("cropped", test_im)
-            # cv2.waitKey()
-
-    #processed = cv2.bitwise_and(processed, im, mask=mask)
     # cv2.imshow("cropped", processed)
     # cv2.waitKey()
 
     return processed
 
-def get_boxes(pil_image):
+def get_boxes(pil_image, limits_h, limits_w, countour_limits):
     im = np.array(pil_image) 
-    scale = 1
-    im = resize_img(im, scale)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     contours = get_countours(im)
-    boxes = crop(im, contours, (500,800), (300,1500)) 
+    boxes = crop(im, contours, limits_h, limits_w) #(500,800), (300,1500)) 
 
     boxes_processed = []
     for b in boxes:
-        # cv2.imshow("cropped", b)
-        # cv2.waitKey()
-        
         contours = get_countours(b)
         boxes_processed.append(
-            remove_contours(b, contours, (60, 400))
+            remove_contours(b, contours, countour_limits)# (60, 400))
         )
 
     return boxes_processed
---
+++ parser/parser.py
@@ -0,0 +1,206 @@
+import os
+from .helpers import get_boxes, pdf_to_img, items_to_csv, strip_lower
+import logging
+import pytesseract
+from collections import OrderedDict
+import re
+
+# import matplotlib.pyplot as plt
+# import keras_ocr
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.FileHandler("parser/debug.log"),
+                              logging.StreamHandler()])
+
+class Parser:
+    BASE_DATA_PATH = 'data/'
+    DPI = 600
+
+    def __init__(self, state, lang, separator= ':', handle = [], separators = [], ommit = None, remove_columns = []):
+        self.state = state.lower()
+        #self.columns = columns
+        self.lang = lang
+        self.separator = separator
+        self.ommit = ommit
+        self.separators = separators
+        self.handle = handle
+        self.remove_columns = remove_columns
+
+        self.output_csv = self.BASE_DATA_PATH + 'out/' + self.state + '/'
+        if not os.path.exists(self.output_csv):
+            os.makedirs(self.output_csv)
+
+    def get_full_path_files(self, path):
+        return [os.path.join(path, f) for f in os.listdir(path)]
+
+    def filter_and_sort(self, objs, ext):
+        result = list(filter(lambda x: x.endswith(ext), objs))
+        result.sort()
+        return result
+
+    def get_this_state_files(self):
+        path = f'{self.BASE_DATA_PATH}in/{self.state}'
+        files_path_list = self.get_full_path_files(path)
+        return self.filter_and_sort(files_path_list, '.pdf')
+
+    def ommit_sentences(self, text):
+        if self.ommit:
+            for o in self.ommit:
+                text = text.replace(o, '')
+            
+        return text
+    
+    def handle_separation_error(self, r, separator, result):
+        last_key = None
+        is_splitted = False
+
+        if '-' in r:
+            rr = r.split('-')
+            key = rr[0]
+            value = '-'.join(rr[1:])
+            result[key] = value
+            last_key = key
+            is_splitted = False
+
+        else:
+            logging.error(f'Separation error : {r}')
+
+        return result, last_key, is_splitted
+
+    def split_2(self, separated):
+        key = strip_lower(separated[0])
+        value = separated[1].strip()
+        return key, value
+
+    def custom_split(self, r, separator, result):
+        is_splitted = False
+
+        # for exceptions
+        #if any([re.findall(f"^[\w]{x}^[\w]", r.lower()) for x in self.handle]):
+        if any([x in r.lower() for x in self.handle]):
+            return self.handle_separation(r, separator, result)
+
+        # normal split
+        else:
+            separated = r.split(separator)
+
+            if len(separated) == 2:
+                key, value = self.split_2(separated)
+                result[key] = value
+                last_key = key
+                is_splitted = True
+
+            else:
+                return self.handle_separation_error(r, separator, result)
+        
+        return result, last_key, is_splitted
+
+     
+    def process_boxes_text(self, text):
+        #logging.info('Processing boxes\' text..')
+        raw = self.ommit_sentences(text)
+        raw = raw.replace('\n\n', '\n').split('\n')
+        result = OrderedDict()
+        
+        # TODO not abstracted
+        id_ = raw.pop(0).strip()
+        first = id_.split(' ')
+   
+        if len(first) > 1:
+            result['count'] = first[0]    
+            result['id'] = first[1]
+        else:
+            result['count'] = ''
+            result['id'] = first[0]
+
+            logging.error(f"Not clear id: {raw}")
+
+        # To add data to previous column
+        last_key = None
+
+        # Iter over results and split with separators
+        for r in raw:
+            is_splitted = False
+            for sep in self.separators:
+                if sep in r.lower():
+                    result, last_key, is_splitted = self.custom_split(r, sep, result)
+
+                    if is_splitted:
+                        break
+
+            if r and not is_splitted:
+                try:
+                    result[last_key] = result[last_key] + ' ' + r.strip()
+                except Exception as e:
+                    logging.error(f'Exception: {e}: {raw}') 
+                    result, last_key, is_splitted = self.handle_separation_error(r, self.separators, result)
+                    
+        return result
+
+    # def handle_extra_pages(self, pages):
+    #     # Overwrite with particular scripts
+    #     return {}, {}
+
+    # def format_items(self, items, first_page_results, last_page_results):
+    #     # Overwrite with particular scripts
+    #     return items
+
+    # def get_header(self, page):
+    #     # Overwrite with particular scripts
+    #     return {}
+
+    def check_columns(self, items):
+        result = []
+        for i in items:
+            for rk in self.remove_columns:
+                if rk in i.keys():
+                    logging.error(f'Removed {k} from {str(i)}')
+                    del i[rk]
+            result.append(i)
+
+        return result
+
+    # def export_to_csv(self, items):
+    #     df = pd.DataFrame.from_dict(items)
+    #     df = self.check_errors(df)
+    #     output_path = self.output_csv + self.state + '.csv'
+    #     df.to_csv (output_path, index = False, header=True) 
+
+
+    def run(self):
+        pdf_files_paths = self.get_this_state_files()
+        if not pdf_files_paths:
+            logging.info(f'No files found for {self.state}')
+
+        for pdf_file_path in pdf_files_paths:
+            logging.info(f'Converting {pdf_file_path} ...')
+            logging.info('Converting pdf to imgs ...')
+            pages = pdf_to_img(pdf_file_path, dpi=self.DPI)#, page=(1,6))
+            items = []
+            filename = pdf_file_path.split('/')[-1].strip('.pdf')
+
+            first_page_results, last_page_results = self.handle_extra_pages(pages)
+            for page in pages[2:-1]:
+                logging.info('Getting boxes..')
+
+                header = self.get_header(page)
+                boxes = get_boxes(page, (500,800), (300,1500), (60, 400))
+                for box in boxes:
+                    # todo get number and id separated
+                    text = pytesseract.image_to_string(box, lang=self.lang, config='--psm 6')
+
+                    #img = keras_ocr.tools.read(box)
+                    #prediction_groups = pipeline.recognize([box])
+                    #keras_ocr.tools.drawAnnotations(image=img, predictions=prediction_groups[0])
+
+                    item = header.copy()
+                    item.update(self.process_boxes_text(text))
+                    items.append(item)
+
+            
+            #items = self.check_columns(items)
+            formatted_items = self.format_items(items, first_page_results, last_page_results)
+            output_path = self.output_csv + filename + '.csv'
+            items_to_csv(formatted_items, output_path)
+            logging.info(f'Converted to csv: {output_path}')
